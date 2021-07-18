713ca5552571c42d1602708593fe14b62857eb54
galibhassan <galib.hassan.0@gmail.com>
2021-07-18 11:06:42 -0400

    Render task title inside Task with text fitting

--- src/components/task/task.js
+++ src/components/task/task.js
@@ -3,26 +3,32 @@ import styles from "./task.module.css";
 import { BIG_BANG, BIG_CRUNCH, BIG_BANG_YEAR } from "../../constants";
 import { getTaskDivHeightWidthTop } from "../../utils/taskUtils";
 import { ViewTypeContext } from "../context/viewTypeContext";
-import {SelectedTaskContext} from "../context/selectedTaskContext"
+import { SelectedTaskContext } from "../context/selectedTaskContext";
+import tinycolor from "tinycolor2";
+import { Textfit } from "react-textfit";
 
 const Task = (props) => {
   const ctx = useContext(ViewTypeContext);
-  const selectedTaskCTX = useContext(SelectedTaskContext)
+  const selectedTaskCTX = useContext(SelectedTaskContext);
 
   const { task, title, containerHeight, containerWidth, data } = props;
-  const { taskHeight, taskWidth, taskPositionTop, taskPositionLeft } =
-    getTaskDivHeightWidthTop({
-      task,
-      containerHeight,
-      containerWidth,
-      viewType: ctx.viewType,
-      BIG_BANG,
-      BIG_CRUNCH,
-      BIG_BANG_YEAR,
-    });
+  const {
+    taskHeight,
+    taskWidth,
+    taskPositionTop,
+    taskPositionLeft,
+  } = getTaskDivHeightWidthTop({
+    task,
+    containerHeight,
+    containerWidth,
+    viewType: ctx.viewType,
+    BIG_BANG,
+    BIG_CRUNCH,
+    BIG_BANG_YEAR,
+  });
 
   const handleClick = (event) => {
-    selectedTaskCTX.setSelectedTask(task)
+    selectedTaskCTX.setSelectedTask(task);
   };
 
   return (
@@ -36,13 +42,32 @@ const Task = (props) => {
         left: taskPositionLeft,
         color: "white",
         backgroundColor: task.color,
-        fontSize: "10px",
+        // fontSize: "10px",
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
       }}
     >
-      {/* {data.absDays} */}
+      <Textfit
+        mode="multi"
+        style={{
+          padding: "10px",
+          writingMode: taskWidth < taskHeight ? "vertical-rl" : "",
+          textOrientation: taskWidth < taskHeight ? "mixed" : "",
+          width: taskWidth,
+          height: taskHeight,
+          textAlign: "center",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          overflow: "hidden",
+          color: tinycolor(task.color).isLight()
+            ? tinycolor(task.color).darken().darken().darken().toString()
+            : tinycolor(task.color).lighten().lighten().lighten().toString(),
+        }}
+      >
+        {task.title}
+      </Textfit>
     </div>
   );
 };
