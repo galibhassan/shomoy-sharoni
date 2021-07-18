f44d31e05206e51d32142f9ac68f17dbcb84b12e
galibhassan <galib.hassan.0@gmail.com>
2021-07-18 06:08:26 -0400

    Insert TaskDetail into SideBar

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,9 +1,16 @@
 import styles from './sidebar.module.css'
+import { TaskDetail } from "../taskDetail/taskDetail";
+import { useContext } from 'react';
+import { SelectedTaskContext } from '../context/selectedTaskContext';
 
 export const SideBar = (props) => {
+  const selectedTaskCtx = useContext(SelectedTaskContext)
   return(
     <div className={styles.sidebar}>
-      Sidebar
+      <div>logo</div>
+      <TaskDetail 
+        {...selectedTaskCtx.selectedTask}
+      />
     </div>
   )
 };
