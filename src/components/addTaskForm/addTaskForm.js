4c3f6ffa5a3ef8b8d487148de701c7b8216ef043
Galib Hassan <galib.hassan.0@gmail.com>
2021-07-21 07:55:01 +0200

    Improve AddTaskForm

--- src/components/addTaskForm/addTaskForm.js
+++ src/components/addTaskForm/addTaskForm.js
@@ -6,41 +6,34 @@ export const AddTaskForm = (props) => {
     <form className={styles.addTaskForm}>
       <div className={styles.decoration}></div>
       <div className={styles.formHeader}>Add Task</div>
-      
+
       <div className={styles.inputsContainer}>
         <div className={styles.inputsContainer_2}>
-          <input
-            className={styles.inputs}
-            type="text"
-            placeholder="Title"
-          />
-          <input
-            className={styles.inputs}
-            type="text"
-            placeholder="Starts at"
-          />
-          <input
-            className={styles.inputs}
-            type="text"
-            placeholder="Ends at"
-          />
-          <input
-            className={styles.inputs}
-            type="date"
-          />
-          <input
-            className={styles.inputs}
-            type="color"
-            placeholder="Color"
-          />
+          <input className={styles.inputs} type="text" placeholder="Title" />
 
-          
+          <div className={styles.timestampContainer}>
+            <div className={styles.inputLabel}>Starts at</div>
+            <input className={styles.inputs} type="time" />
+          </div>
+          <div className={styles.timestampContainer}>
+            <div className={styles.inputLabel}>Ends at</div>
+            <input className={styles.inputs} type="time" />
+          </div>
+
+          <div className={styles.timestampContainer}>
+            <div className={styles.inputLabel}>Date</div>
+            <input className={styles.inputs} type="date" />
+          </div>
+
+          <div className={styles.timestampContainer}>
+            <div className={styles.inputLabel}>Pick a color</div>
+            <input className={styles.inputs} type="color" placeholder="Color" />
+          </div>
         </div>
 
         <button type="submit" className={styles.addTaskButton}>
           Add Task
         </button>
-        
       </div>
     </form>
   );
