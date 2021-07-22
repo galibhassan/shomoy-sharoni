be591d03749583fb82ced55cfa1a77f8311463be
Galib Hassan <galib.hassan.0@gmail.com>
2021-07-22 07:59:34 +0200

    Add AddTaskForm submit handler

--- src/components/addTaskForm/addTaskForm.js
+++ src/components/addTaskForm/addTaskForm.js
@@ -1,7 +1,29 @@
 import styles from "./addTaskForm.module.css";
-import { FaFacebook, FaGooglePlus, FaGithub } from "react-icons/fa";
+import { useState, useEffect } from "react";
 
 export const AddTaskForm = (props) => {
+  const [taskData, setTaskData] = useState({});
+  useEffect(() => {
+    console.log(taskData);
+  }, [taskData]);
+
+  const handleInputs = (event) => {
+    const inputKey = event.target.name;
+    setTaskData((prevTaskData) => {
+      return {
+        ...taskData,
+        [inputKey]: event.target.value,
+      };
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    // Here, post request should be sent
+    
+  };
+
   return (
     <form className={styles.addTaskForm}>
       <div className={styles.decoration}></div>
@@ -9,29 +31,60 @@ export const AddTaskForm = (props) => {
 
       <div className={styles.inputsContainer}>
         <div className={styles.inputsContainer_2}>
-          <input className={styles.inputs} type="text" placeholder="Title" />
+          <input
+            className={styles.inputs}
+            type="text"
+            placeholder="Title"
+            name="title"
+            onChange={handleInputs}
+          />
 
           <div className={styles.timestampContainer}>
             <div className={styles.inputLabel}>Starts at</div>
-            <input className={styles.inputs} type="time" />
+            <input
+              className={styles.inputs}
+              type="time"
+              name="startsAt"
+              onChange={handleInputs}
+            />
           </div>
           <div className={styles.timestampContainer}>
             <div className={styles.inputLabel}>Ends at</div>
-            <input className={styles.inputs} type="time" />
+            <input
+              className={styles.inputs}
+              type="time"
+              name="endsAt"
+              onChange={handleInputs}
+            />
           </div>
 
           <div className={styles.timestampContainer}>
             <div className={styles.inputLabel}>Date</div>
-            <input className={styles.inputs} type="date" />
+            <input
+              className={styles.inputs}
+              type="date"
+              name="date"
+              onChange={handleInputs}
+            />
           </div>
 
           <div className={styles.timestampContainer}>
             <div className={styles.inputLabel}>Pick a color</div>
-            <input className={styles.inputs} type="color" placeholder="Color" />
+            <input
+              className={styles.inputs}
+              type="color"
+              placeholder="Color"
+              name="color"
+              onChange={handleInputs}
+            />
           </div>
         </div>
 
-        <button type="submit" className={styles.addTaskButton}>
+        <button
+          type="submit"
+          className={styles.addTaskButton}
+          onClick={handleSubmit}
+        >
           Add Task
         </button>
       </div>
